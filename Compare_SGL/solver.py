1d9a348bb1bbc7f9c3ff05f98ac5a44e3a19c5ab
lkgv <lkgv@foxmail.com>
2017-07-17 01:53:52 +0800

    Add files via upload

--- Compare_SGL/solver.py
+++ Compare_SGL/solver.py
@@ -1,25 +1,49 @@
 import commands
+import threading
+import argparse
 import os
+import time
 
-for ypheno_root, ypheno_dirs, files in os.walk("./data/pheno", topdown=False):
-    print 'Current dir: ' + ypheno_root
-    for ypheno_file_name in files:
-        if ypheno_file_name[-10:-4] != 'result' and ypheno_file_name[-4:] == '.csv':
-            print 'Current ypheno file: ' + ypheno_file_name
 
-            root = ypheno_root[-3:] if ypheno_root[-3:-1] == 'ex' else ''
-            file = os.path.join(root, ypheno_file_name)
+def excec(comm):
+    result = commands.getoutput(comm)
+    state_file = open('data/' + comm.split()[2].replace('.csv',
+                                                        '{0}_stat.txt'.format(
+                                                            '_PCA' if comm.split()[3] == 'true' else '')), 'w')
+    state_file.write(str(result))
+    state_file.close()
 
-            print '==================================='
-            print '----Has--PCA'
-            t = commands.getoutput('python test_cv_0.py {0} true'.format(file))
-            print t
-            if 'error' in t.lower():
-                break
-            print '==================================='
-            print '----NO--PCA'
-            t = commands.getoutput('python test_cv_0.py {0} false'.format(file))
-            print t
-            if 'error' in t.lower():
-                break
-            print '==================================='
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', '-d', default='data/pheno',
+                        help='the root path of the ypheno files, default is data/pheno/')
+    parser.add_argument('--use-group-lasso', '-g', default='true',
+                        help='whether use group lasso, default is true, value is true/false')
+    parser.add_argument('--use-lasso', '-l', default='true',
+                        help='whether use lasso, default is true, value is true/false')
+    args = parser.parse_args()
+
+    threads = []
+
+    for ypheno_root, ypheno_dirs, files in os.walk(args.dir, topdown=False):
+        for ypheno_file_name in files:
+            if ypheno_file_name[-10:-4] != 'result' and ypheno_file_name[-4:] == '.csv':
+                root = ypheno_root[5:]
+                file = os.path.join(root, ypheno_file_name)
+
+                for choice in ['true', 'false']:
+                    t = threading.Thread(target=excec,
+                                         args=('python test_cv_0.py {0} {1}'.format(
+                                             file, choice),))
+                    print 'python test_cv_0.py {0} {1}'.format(
+                        file, choice)
+                    threads.append(t)
+
+    p = threads[0]
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+        p = t
+    p.join()
+    print "all over %s" % time.ctime()
